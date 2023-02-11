86828855916b9723b7d574616e3fe46c79cb5c16
RareTC <trevorjcampbell@aol.com>
2023-02-11 10:21:02 -0800

    exported routers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 var methodOverride = require('method-override');
 
 require('dotenv').config();
@@ -10,7 +11,9 @@ require('./config/database');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var ideasRouter = require('./routes/ideas');
+var reviewsRouter = require('./routes/reviews');
+
 
 var app = express();
 
@@ -22,11 +25,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', ideasRouter);
+app.use('/', reviewsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
