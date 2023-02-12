c3afdbe92d2700d00f37f632b68395d7a4709c96
RareTC <trevorjcampbell@aol.com>
2023-02-12 11:16:21 -0800

    Updated code for nav avatar

---
+++ routes/user.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// router.get('/auth/google', passport.authenticate(
+//     // Which passport strategy is being used?
+//     'google',
+//     {
+//       scope: ['profile', 'email'],
+//       // Optional
+//       prompt: 'select_account'
+//     }
+//   ));
+  
+// router.get('/oauth2callback', passport.authenticate(
+//     'google',
+//     {
+//         successRedirect: '/movies',
+//         // Change to what's best for YOUR app
+//         failureRedirect: '/movies'
+//     }
+//     ));
+
+module.exports = router;
